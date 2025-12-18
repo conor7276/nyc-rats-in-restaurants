1073ed9b4d6ae198f07278afe37c833d39ebf6c8
conor7276 <conor7276@gmail.com>
2025-12-17 21:26:38 -0500

    Adding ability to pull and save data

--- scripts/pull_rodent_data.py
+++ scripts/pull_rodent_data.py
@@ -5,25 +5,44 @@ import logging
 import os
 from sodapy import Socrata
 from dotenv import load_dotenv, dotenv_values
+from datetime import date
 
 env_path = Path(__file__).resolve().parent.parent / "secrets.env"
 env_vars = dotenv_values(env_path)
 print(env_path)
 print(env_vars)
 
-domain = 'https://data.cityofnewyork.us'
-dataset_id = 'p937-wjvj'
-endpoint = 'https://data.cityofnewyork.us/api/v3/views/p937-wjvj/query.json'
-# USE POST when requesting
-print(requests.post(endpoint))
+# do not use an https
+DOMAIN = 'data.cityofnewyork.us'
+DATASET_ID = 'p937-wjvj'
+
+
+# APP_TOKEN_API_KEY
 client = Socrata(
-    domain,
-    app_token = env_vars['API_KEY'],
+    DOMAIN,
+    app_token = env_vars['APP_TOKEN_API_KEY'],
     timeout = 10
 )
 
 print(client)
 
-data = client.get(dataset_id, limit = 2)
+monday_last_week = date(2025,12,8).isoformat()
+sunday_last_week = date(2025,12,14).isoformat()
 
-print(data)
+data = client.get(
+    DATASET_ID,
+    select = """*""",
+    where = f"""
+        inspection_date >= '{monday_last_week}' AND
+        inspection_date <= '{sunday_last_week}' AND
+        result != 'Passed'
+    """,
+    limit =10)
+
+# print(data)
+
+import pandas as pd
+
+data = pd.DataFrame(data)
+print(data.head())
+data.to_csv("data.csv", index = False)
