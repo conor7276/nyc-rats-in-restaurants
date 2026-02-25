5b53c4ddc713d9e84e326792adac22c4f7077e91
conor7276 <conor7276@gmail.com>
2026-02-24 22:27:49 -0500

    [ADD] Added caching and optimized rats

--- frontend/pages/Rat_Map.py
+++ frontend/pages/Rat_Map.py
@@ -3,65 +3,85 @@ from streamlit_folium import st_folium
 import pandas as pd
 import numpy as np
 import folium
+from folium.plugins import MarkerCluster
 from pathlib import Path
 
 
 
 def app() -> None:
 
+    # @st.cache_resource
+    def build_map(df):
+
+        # df = df.head(50)
+
+
+        rat_map = folium.Map(
+            location = [40.7, -74.05],
+            tiles = "cartodb positron"
+        )
+
+        marker_cluster = MarkerCluster().add_to(rat_map)
+        rat_icon = folium.CustomIcon(
+            icon_image = "icons/rat-emoji_transparent.png",
+            icon_size = (25,25),
+            icon_anchor = (12,12)
+        )
+
+        
+        # add icons
+        for row in df.itertuples(index=False):
+            html = f"""
+            <div style="font-family: Arial; font-size: 13px; width: 220px;">
+                <b>{row.restaurant_name}</b><br>
+                {row.address}<br>
+                <hr style="margin: 5px 0;">
+                <b>Inspection Result:</b> {row.result}<br>
+                <b>Date:</b> {row.inspection_date}
+            </div>
+            """
+
+            # popup = folium.Popup(html, max_width=300)
+
+            folium.Marker(
+                [row.latitude, row.longitude],
+                icon = rat_icon,
+                popup = html
+            ).add_to(marker_cluster)
+
+        return rat_map
+
     st.header("We got rats at the function")
     
     # with st.sidebar:
         
     # Temporary fixed data will have to figure out databricks one day
-    data_path = Path(__file__).resolve().parent.parent.parent / "data" / "intermediate_data" / "data_2025-12-15_2025-12-21.csv"
+    data_path = Path(__file__).resolve().parent.parent.parent / "data" / "final_data" / "aggregated_rat_data.csv"
 
     df = pd.read_csv(data_path)
 
-    cols = ['latitude', 'longitude', 'address', 'displayName']
+    df = df.drop_duplicates()
 
-    df = df[cols]
+    # st.dataframe(df)
 
-    rat_map = folium.Map(
-        location = [40.7, -74.05],
-        tiles = "cartodb positron"
-    )
+    # cols = ['latitude', 'longitude', 'address', 'address']
 
+    # df = df[cols]
 
-    # add icons
-    for row in df.iterrows():
-
-        rat_icon = folium.CustomIcon(
-        icon_image = "icons/rat-emoji_transparent.png",
-        icon_size = (25,25),
-        icon_anchor = (12,12)
-        )
-
-        # html = f"""
-        # <div style="font-family: Arial; font-size: 13px; width: 220px;">
-        #     <b>{row[1]['restaurant_name']}</b><br>
-        #     {row[1]['address']}<br>
-        #     <hr style="margin: 5px 0;">
-        #     <b>Inspection Result:</b> {row[1]['result']}<br>
-        #     <b>Date:</b> {row[1]['inspection_date']}
-        # </div>
-        # """
-
-        # popup = folium.Popup(html, max_width=300)
-
-        folium.Marker(
-            [row[1]['latitude'], row[1]['longitude']],
-            icon = rat_icon,
-            # popup = popup
-        ).add_to(rat_map)
-
+    rat_map = build_map(df)
 
     st.write("Below is a map of the restaurants that failed rat inspections within the past week.")
-    st_folium(rat_map, height = 500, width = 700)
+
+
+    st_folium(
+        rat_map,
+        use_container_width= True,
+        returned_objects = []
+        # height = 500,
+        # width = 700
+    )
 
     
-
-    st.dataframe(df)
     st.write("Datasets included below. \n NYC Open Data Rodent Inspeciton Dataset.")
 
 # idk why it doesn't work without this my other streamlit apps never needed this
