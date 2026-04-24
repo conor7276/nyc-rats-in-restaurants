3a67a0010002c0c229b71e0d6d48bebb95f10737
conor7276 <conor7276@gmail.com>
2026-04-24 11:59:26 -0400

    Deleting older google api's script and using new script

--- scripts/pull_nearby_locations.py
+++ scripts/pull_nearby_locations.py
@@ -1,14 +1,17 @@
 import pandas as pd
-from pathlib import Path
-from dotenv import dotenv_values
-from fuzzywuzzy import fuzz
+import numpy as np
 import requests
 import time
-import logging
+import json
+from dotenv import load_dotenv, dotenv_values
+from pathlib import Path
 import os
+from fuzzywuzzy import fuzz
 import argparse
+import logging
 from datetime import datetime
 
+# Read in environment variables
 parser = argparse.ArgumentParser()
 parser.add_argument("--start-date", required=True)
 parser.add_argument("--end-date", required=True)
@@ -16,6 +19,8 @@ parser.add_argument("--manual-start-date", default = "")
 parser.add_argument("--manual-end-date", default = "")
 parser.add_argument("--dry-run", default="false")
 args = parser.parse_args()
+
+geoapify_key = os.getenv('GEOAPIFY_KEY')
 
 # Set up logger
 logging.basicConfig(level = logging.INFO, format = '%(message)s')
@@ -33,6 +38,7 @@ else:
     end_date = datetime.strptime(args.end_date, "%Y-%m-%d").date().isoformat()
     logger.info(f"Using auto run dates {start_date} and {end_date}")
 
+
 # establish directories
 input_dir = Path("data/raw_data")
 input_dir.mkdir(parents=True, exist_ok=True)
@@ -41,15 +47,12 @@ output_dir.mkdir(parents=True, exist_ok=True)
 input_filepath = input_dir / f"data_{start_date}_{end_date}.csv"
 output_filepath = output_dir / f"data_{start_date}_{end_date}.csv"
 
-# Get google api key
-API_KEY = os.getenv('GOOGLE_PLACES_API_KEY')
-
 logger.info("Data and environmnet variable paths loaded")
 
 # Read data and environment variables
-df = pd.read_csv(input_filepath)
-# env_vars = dotenv_values(env_path)
-logger.info("Data and environment variables loaded.")
+df = pd.read_csv(input_filepath, parse_dates= ['inspection_date', 'approved_date'])
+
+logger.info("Reading in file.")
 
 # Filter out bad coordinates
 df = df.dropna(subset = ['longitude', 'latitude'])
@@ -59,118 +62,157 @@ df = df[(df['longitude'] != 0) & (df['latitude'] != 0)]
 df['street_name'] = df['street_name'].apply(lambda x : x.title())
 df['address'] = df.apply(lambda x : str(x['house_number']) + ' ' + x['street_name'] ,axis = 1)
 
-logger.info("Raw data cleaned")
+# Turn dates from timestamp level to date level
+df['inspection_date'] = df['inspection_date'].apply(lambda x : pd.Timestamp(year = x.year, month = x.month, day = x.day))
+df['approved_date'] = df['approved_date'].apply(lambda x : pd.Timestamp(year = x.year, month = x.month, day = x.day))
 
+# Select needed columns
+df = df[
+    ['inspection_type',
+     'job_id',
+     'job_progress',
+     'house_number',
+     'street_name',
+     'address',
+     'zip_code',
+     'latitude',
+     'longitude',
+     'result',
+     'inspection_date',
+     'approved_date',
+     'nta'
+     ]
+]
 
-# The endpoint for the Nearby Search (New) API
-url = "https://places.googleapis.com/v1/places:searchNearby"
+# Differentiate rodent data columns from API columns
+df.columns = df.columns.str.lower() + '_interdata'
 
-# Define the request body as a Python dictionary
-# This example searches for restaurants within a 500-meter radius
-# around a specific latitude and longitude.
-request_body = {
-    "includedTypes": ["restaurant", "meal_delivery", "meal_takeaway", "bakery", "bar", "cafe"],
-    "maxResultCount": 20,
-    "locationRestriction": {
-        "circle": {
-            "center": {
-                "latitude": 0,
-                "longitude": 0
-            },
-            "radius": 25.0 # Turn back to 25
-        }
-    }
-}
-# Headers determine what contents of the restaurants are returned
-headers = {
-    "Content-Type": "application/json",
-    "X-Goog-Api-Key": API_KEY,
-    "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.location,places.rating"
-}
+################################## Perform API actions #######################################
 
-logger.info(f"Beginning to make requests for each location from {start_date} to {end_date}")
-requested_places = pd.DataFrame()
+logger.info("Beginning API calls to get nearby locations.")
 
-# make requests looping through each row
-for row in df.iterrows():
+# Set API params
+categories = "catering.restaurant,commercial.food_and_drink,catering.fast_food,catering.food_court,catering.bar"
+radius = "50" # meters
+max_locations_returned = "5"
 
-    temp_df = pd.DataFrame()
+# Create new dataframe to store new data
+all_restaurants_df = pd.DataFrame()
 
-    latitude = row[1]['latitude']
-    longitude = row[1]['longitude']
-    address = row[1]['address']
-    inspection_type = row[1]['inspection_type']
-    inspection_date = row[1]['inspection_date']
-    result = row[1]['result']
-    
+# Get each coordinates from each row
+for _ , row in df.iterrows():
+
+    time.sleep(0.5)
+
+    # Get coordinates for parameters
+    longitude = str(row['longitude_interdata'])
+    latitude = str(row['latitude_interdata'])
+
+    # Build url
+    url = f"https://api.geoapify.com/v2/places?categories={categories}&filter=circle:{longitude},{latitude},{radius}&bias=proximity:{longitude},{latitude}&limit={max_locations_returned}&apiKey={geoapify_key}"
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    res = json.loads(response.text)
+
     try:
-        # Makes POST request to google searchNearby API and returns json of nearby locations
+        local_restaurant_df = pd.DataFrame([
+            {
+                **feature["properties"],
+                "geo_lon": feature["geometry"]["coordinates"][0],
+                "geo_lat": feature["geometry"]["coordinates"][1],
+            }
+            for feature in res["features"]
+        ])
 
-        # Reset for every iteration
-        request_body['locationRestriction']['circle']['center']['latitude'] = latitude
-        request_body['locationRestriction']['circle']['center']['longitude'] = longitude
-
-        # Make request
-        response = requests.post(url, headers=headers, json=request_body)
-        time.sleep(0.25)
-
-        # Success == 200
-        if response.status_code == 200:
-            response_data = response.json()
-            if response_data and "places" in response_data:
-                for place in response_data["places"]:
-                    display_name = place.get("displayName", {}).get("text", "N/A")
-                    formatted_address = place.get("formattedAddress", "N/A")
-                    location = place.get("location", {})
-                    rating = place.get("rating", "N/A")
-            else:
-                pass
-        else:
-            logger.error(f"Request failed with {response.status_code} at {row[0]} in data check Logger and {response.text} then rerun.")
-
-    except requests.exceptions.RequestException as e:
-        logger.error(f"Request failed with {response.status_code} at {row[0]} in data check Logger and {response.text} then rerun.")
-        print(f"An error occurred: {e}")
-    
-    # Append to main dataframe if data is returned
-    if response_data:
-        temp_df = pd.DataFrame(response_data['places'])
-        temp_df['latitude'] = location['latitude']
-        temp_df['longitude'] = location['longitude']
-        temp_df['restaurant_name'] = display_name
-        temp_df['address'] = formatted_address
-        temp_df['rating'] = rating
-        temp_df['inspection_type'] = inspection_type
-        temp_df['inspection_date'] = inspection_date
-        temp_df['result'] = result
+        local_restaurant_df['inspection_type_interdata'] = row['inspection_type_interdata']
+        local_restaurant_df['job_id_interdata'] = row['job_id_interdata']
+        local_restaurant_df['job_progress_interdata'] = row['job_progress_interdata']
+        local_restaurant_df['house_number_interdata'] = row['house_number_interdata']
+        local_restaurant_df['street_name_interdata'] = row['street_name_interdata']
+        local_restaurant_df['address_interdata'] = row['address_interdata']
+        local_restaurant_df['zip_code_interdata'] = row['zip_code_interdata']
+        local_restaurant_df['latitude_interdata'] = row['latitude_interdata']
+        local_restaurant_df['longitude_interdata'] = row['longitude_interdata']
+        local_restaurant_df['result_interdata'] = row['result_interdata']
+        local_restaurant_df['inspection_date_interdata'] = row['inspection_date_interdata']
+        local_restaurant_df['approved_date_interdata'] = row['approved_date_interdata']
+        local_restaurant_df['nta_interdata'] = row['nta_interdata']
         
-        requested_places = pd.concat([requested_places, temp_df])
-    else:
-        pass
-logger.info("Requestes finished and data combined.")
+        all_restaurants_df = pd.concat([all_restaurants_df, local_restaurant_df])
+    except Exception as e:
+        # Move on if no data is found
+        continue
 
-# Check for if any data is returned at all
-if requested_places.empty == False:
-    requested_places_saved = requested_places.copy()
+logger.info("API calls completed, beginning data processing.")
 
-    # Compare ratios between addresses to get correct original location
-    requested_places_saved['ratio'] = requested_places_saved.apply(lambda x : fuzz.WRatio(x['formattedAddress'], x['address']), axis = 1)
-    requested_places_saved = requested_places_saved[requested_places_saved['ratio'] >= 80]
+if all_restaurants_df.empty:
+    # If no data was pulled
+    logger.info("No locations were able to be found.")
+    exit(0)
 
-    # Check number in address for final comparison
-    requested_places_saved['split_check'] = requested_places_saved.apply(lambda x : x['formattedAddress'].split()[0] == x['address'].split()[0], axis = 1)
-    requested_places_saved = requested_places_saved[requested_places_saved['split_check'] == True]
+# Process data
+selected_columns = [
+    'name', 'county', 'city', 'postcode', 'district', 'suburb', 'housenumber', 'street', 'address_line2',
+    'lon','lat', 'formatted', 'catering', 'commercial','house_number_interdata', 'street_name_interdata', 'address_interdata',
+    'inspection_type_interdata', 'result_interdata', 'inspection_date_interdata', 'approved_date_interdata', 'nta_interdata'
+    ]
 
-    requested_places_saved = requested_places_saved.drop(columns = ["ratio", "split_check", "displayName", "formattedAddress", "location"])
+# format address
+geo_rest_df = all_restaurants_df.copy()
+geo_rest_df['address_line2'] = geo_rest_df['address_line2'].apply(lambda x : x.split(',')[0].strip() if x else None)
+geo_rest_df = all_restaurants_df[selected_columns]
 
-    requested_places_saved[['longitude', 'latitude']] = requested_places_saved[['longitude', 'latitude']].round(6)
+requested_places_saved = geo_rest_df.copy()
 
-    requested_places_saved.to_csv(output_filepath, index = False)
-    
-    logger.info("Preview of dataframe: ")
-    logger.info(requested_places_saved.head())
-    logger.info("Address comparison and cleanup completed. File Saved to intermediate data folder.")
-else:
-    # if no data is returned at all
-    logger.info("No Data was able to be found.")
-    print("No locations were found that failed inspection. Check Logs!")
+# Logic is fuzzy with addresses we try to pick ones that are close to matching then compare numerical addresses
+requested_places_saved['ratio'] = requested_places_saved.apply(lambda x: fuzz.WRatio(x['address_line2'], x['address_interdata']), axis=1)
+requested_places_saved = requested_places_saved[requested_places_saved['ratio'] >= 80]
+
+requested_places_saved['split_check'] = requested_places_saved.apply(lambda x : x['housenumber'] == x['house_number_interdata'], axis = 1)
+requested_places_saved = requested_places_saved[requested_places_saved['split_check'] == True]
+
+# Column cleanup
+requested_places_saved[['lon', 'lat']] = requested_places_saved[['lon', 'lat']].round(6)
+selected_columns = [
+    'name', 'county', 'city', 'postcode', 'suburb', 'address_line2', 'address_interdata', 'lon', 'lat',
+    'catering', 'commercial', 'inspection_type_interdata', 'inspection_date_interdata', 'approved_date_interdata', 'result_interdata', 'nta_interdata'
+]
+requested_places_saved = requested_places_saved[selected_columns]
+requested_places_saved = requested_places_saved.rename(
+    columns = {
+        'address_line2' : 'address',
+        'inspection_type_interdata' : 'inspection_type',
+        'inspection_date_interdata' : 'inspection_date',
+        'approved_date_interdata' : 'approved_date',
+        'result_interdata' : "result",
+        'nta_interdata' : 'neighborhood'}
+)
+
+def category_handler(catering, commercial):
+    if pd.notnull(catering): # If it is a restaurant it will show the type of catering
+        try:
+            return catering['cuisine'].title().strip()
+        except Exception as e:
+            return 'N/A'
+    else: # If it is not a restaurant it will show what other type of place it is eg: bar/supermarket
+        try:
+            return commercial['type'].title().strip()
+        except Exception as e:
+            return 'N/A'
+        
+requested_places_saved['type'] = requested_places_saved.apply(lambda x : category_handler(x['catering'], x['commercial']), axis = 1)
+requested_places_saved = requested_places_saved.drop(columns = ['catering', 'commercial'])
+
+# Handle addresses
+requested_places_saved['address'] = requested_places_saved['address'].apply(lambda x : x.split(',')[0])
+requested_places_saved = requested_places_saved.drop(columns = ['address_interdata'])
+
+logger.info("Saving data to repo.")
+
+requested_places_saved.to_csv(output_filepath, index = False)
+
+logger.info(f"Data was successfully saved to repo. Preview of dataframe: {requested_places_saved.head()}")
+
